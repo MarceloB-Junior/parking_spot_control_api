8d63857b593f666fe98d67a0d60f99f543bb7b03
MarceloB-Junior <marcelo.bernardino@sou.unifal-mg.edu.br>
2024-11-18 00:29:39 -0300

    Updating the 'dtos' package and controller methods.

---
+++ parking-spot-control-api/parking-control/src/main/java/com/api/parking_control/dtos/ParkingSpotDto.java
@@ -0,0 +1,16 @@
+package com.api.parking_control.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ParkingSpotDto(
+        @NotBlank String parkingSpotNumber,
+        @NotBlank @Size(max = 7) String licensePlateCar,
+        @NotBlank String brandCar,
+        @NotBlank String modelCar,
+        @NotBlank String colorCar,
+        @NotBlank String responsibleName,
+        @NotBlank String apartment,
+        @NotBlank String block
+) {
+}
